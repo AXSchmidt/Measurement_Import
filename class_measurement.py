fa3b0ce371ff5b2c0c53aededaee9f4012b59f3a
McBabba <57316541+McBabba@users.noreply.github.com>
2019-11-03 19:58:43 +0100

    Create class_measurement.py

---
+++ class_measurement.py
@@ -0,0 +1,5 @@
+class Measurement:
+    def __init__(self, measurementTime, measurementValue, measurementType):
+        self.measurementTime = measurementTime
+        self.measurementValue = measurementValue
+        self.measurementType = measurementType

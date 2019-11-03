05746e8eab2dd3ac01b5494ae03396bd20da8575
McBabba <57316541+McBabba@users.noreply.github.com>
2019-11-03 21:39:42 +0100

    implement class Sampling

---
+++ script_import.py
@@ -0,0 +1,18 @@
+class Sampling:
+    
+    def __init__(self):
+        self.data = []
+        
+    def test:
+        import("measurement_data.json")
+        sample("2017-01-01 00:00:00", self.data)
+        
+    def import(file):
+        this.data.clear()
+        f = open(file, "r")
+        for x in f:
+            print(x)
+            self.data.append(x)
+        
+    def sample(startOfSampling, unsampledMeasurement):
+        print(a + b)

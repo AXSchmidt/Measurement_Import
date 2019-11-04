50b7fbe304c677ff7e0540a46da19ed1f3f86851
McBabba <57316541+McBabba@users.noreply.github.com>
2019-11-04 09:50:51 +0100

    implement first import functions

--- class_measurement.py
+++ class_measurement.py
@@ -2,4 +2,7 @@ class Measurement:
     def __init__(self, measurementTime, measurementValue, measurementType):
         self.measurementTime = measurementTime
         self.measurementValue = measurementValue
-        self.measurementType = measurementType
+        self.measurementType = measurementType    
+        
+    def toString(self):
+        return self.measurementTime + ", " + self.measurementType + ", " + self.measurementValue
--- script_import.py
+++ script_import.py
@@ -1,18 +1,53 @@
+import MeasurementType
+import Measurement
+
 class Sampling:
     
     def __init__(self):
         self.data = []
         
-    def test:
-        import("measurement_data.json")
-        sample("2017-01-01 00:00:00", self.data)
+    def test_file(self):
+        self.importfile("measurement_data.json")
+        return(self.sample("2017-01-01 00:00:00", self.data))
         
-    def import(file):
-        this.data.clear()
+    def test(self):
+        self.data.clear()
+        self.data.append("{2017-01-03T10:04:45, TEMP, 35.79}")
+        self.data.append("{2017-01-03T10:01:18, SPO2, 98.78}")
+        self.data.append("{2017-01-03T10:09:07, TEMP, 35.01}")
+        return(self.sample("2017-01-01 00:00:00", self.data))
+        
+    # Convert String to Class Measurement
+    def strToMeasurement(self, string):
+        while string[0] == "{":
+            string = string[1:]
+        while string[-1] == "}":
+            string = string[:-1]
+        li = list(string.split(", "))      
+        if len(li) >= 3:
+            return Measurement(li[0], li[2], li[1])
+        else:
+            return Measurement("1900-01-01 00:00:00", "", "")
+        
+    # imports a file with sample data into the object    
+    def importfile(self, file):
+        self.data.clear()
         f = open(file, "r")
         for x in f:
-            print(x)
             self.data.append(x)
         
-    def sample(startOfSampling, unsampledMeasurement):
-        print(a + b)
+    # main function: samples data    
+    def sample(self, startOfSampling, unsampledMeasurement):
+        # get all measurement types
+        print(unsampledMeasurement)
+        output = []
+        measurement_types = MeasurementType()
+        for m_type in measurement_types.measurementTypes:
+            output.append([m_type, []])
+    	# insert unsampledMeasurement
+        for uMeasurement in unsampledMeasurement:
+            measurement = self.strToMeasurement(uMeasurement)
+            for i in range(len(output)):
+                if output[i][0] == measurement.measurementType:
+                	output[i][1].append(measurement)
+        return output

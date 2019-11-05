bad2a09785f7fb3a1584f73b16546c3bc5b83524
McBabba <57316541+McBabba@users.noreply.github.com>
2019-11-05 09:35:18 +0100

    added unittests

--- test_script_import.py
+++ test_script_import.py
@@ -1,11 +1,54 @@
 import unittest
+from datetime import datetime
 from script_import import Sampling
 from class_measurement import Measurement
 
 class TestSample(unittest.TestCase):
     
+    # checks if a correct measurement object is returned
+    def test__strToMeasurement(self):
+        sampling = Sampling()
+        measurement = sampling.strToMeasurement("{2019-11-01T10:14:45, TEMP, 1}")
+        result = Measurement("2019-11-01T10:14:45", "1", "TEMP")
+        self.assertEqual(measurement.toString(), result.toString())
+    
+    # checks if the default object is returned if the input is incorrect
+    def test__strToMeasurement_wrong_imput(self):
+        sampling = Sampling()
+        measurement = sampling.strToMeasurement("{2019-11-01T10:14:45, TEMP, 1, 1}")
+        result = Measurement("1900-01-01T00:00:00", "", "")
+        self.assertEqual(measurement.toString(), result.toString())
+        
+    # checks if a string is properly converted into a datetime
+    def test__strToDateTime(self):
+        sampling = Sampling()
+        datetime_object = sampling.strToDateTime("2019-11-01T10:14:45")
+        result = datetime(2019, 11, 1, 10, 14, 45)
+        self.assertEqual(datetime_object, result)
+        
+    # checks if a string is converted into the default datetime if the input is incorrect
+    def test__strToDateTime_wrong_imput(self):
+        sampling = Sampling()
+        datetime_object = sampling.strToDateTime("20000019-11-01T10:14:45")
+        result = datetime(1900, 1, 1, 0, 0, 0)
+        self.assertEqual(datetime_object, result)
+        
+    # checks if the datetime is rounded up correctly
+    def test__roundTime(self):
+        sampling = Sampling()
+        datetime_object = sampling.roundTime(datetime(2019, 11, 1, 10, 14, 45))
+        result = datetime(2019,11,1,10,15)
+        self.assertEqual(datetime_object, result)
+       
+    # checks if the datetime is returned correctly if there is nothing to round
+    def test__roundTime_equal(self):
+        sampling = Sampling()
+        datetime_object = sampling.roundTime(datetime(2019, 11, 1, 10, 15, 0))
+        result = datetime(2019,11,1,10,15)
+        self.assertEqual(datetime_object, result)
+    
     # does the algorithm sort correctly according to the time 
-    def test_sort(self):
+    def test__sample_sort(self):
          # prepare input data
         data = []
         data.append("{2019-11-01T10:14:45, TEMP, 3}")
@@ -26,7 +69,7 @@ class TestSample(unittest.TestCase):
         self.assertEqual(result, output)
         
     # does the algorithm ignore wrong / incorrect measurement_types
-    def test_wrong_type(self):
+    def test__sample_wrong_type(self):
          # prepare input data
         data = []
         data.append("{2019-11-01T10:14:45, TMP, 1}")
@@ -42,7 +85,7 @@ class TestSample(unittest.TestCase):
         self.assertEqual(result, output)
         
     # does the algorithm ignore wrong / incorrect measurement_datetime
-    def test_wrong_date(self):
+    def test__sample_wrong_date(self):
          # prepare input data
         data = []
         data.append("{200019-11-01T10:14:45, TEMP, 1}")
@@ -58,7 +101,7 @@ class TestSample(unittest.TestCase):
         self.assertEqual(result, output)        
         
     # complex test
-    def test_complex(self):
+    def test__sample_complex(self):
         # prepare input data
         data = []
         data.append("{2017-01-03T10:04:45, TEMP, 35.79}")
